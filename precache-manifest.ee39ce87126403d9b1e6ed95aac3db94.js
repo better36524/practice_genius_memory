7ae4fff7644519e7d6522f8c19bb7dfc5c65eaf1
better36524 <better36524@gmail.com>
2019-12-11 20:24:55 +0900

    Updates

---
+++ precache-manifest.ee39ce87126403d9b1e6ed95aac3db94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3dcf3d742af62adbedccec34b1b8cb2",
+    "url": "/practice_genius_memory/index.html"
+  },
+  {
+    "revision": "b7b5466892d214e1c6e6",
+    "url": "/practice_genius_memory/static/css/main.4d0d92cd.chunk.css"
+  },
+  {
+    "revision": "80003de8d6d064508fae",
+    "url": "/practice_genius_memory/static/js/2.6f9a32ef.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/practice_genius_memory/static/js/2.6f9a32ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7b5466892d214e1c6e6",
+    "url": "/practice_genius_memory/static/js/main.d9bff59e.chunk.js"
+  },
+  {
+    "revision": "5fed444e8dc0bde45e71",
+    "url": "/practice_genius_memory/static/js/runtime-main.3dce5363.js"
+  }
+]);
